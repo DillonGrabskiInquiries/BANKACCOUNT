3e199e965165088f4735eb26743b2a47aa1529ce
DillonGrabskiInquiries <sahasrahlainquiries@gmail.com>
2021-06-10 22:40:37 -0500

    4final

--- bank.py
+++ bank.py
@@ -1,10 +1,25 @@
-class BankAccount:
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount(int_rate=0.02, balance=0)
+    
+    def make_deposit(self, amount):
+        self.account.balance += amount
+        return self
 
+    def make_withdrawal(self, amount):
+        self.account.balance -= amount
+        return self
+
+    def display_user_balance(self):
+        print(self.account.balance)
+        return self
+
+class BankAccount:
     def __init__(self, int_rate, balance): 
-        self.balance = balance
         self.int_rate = int_rate
-        int_rate = 0
-        balance = 0
+        self.balance = balance
 
     def deposit(self, amount):
         self.balance += amount
@@ -14,17 +29,14 @@ class BankAccount:
         self.balance -= amount
         return self
 
-    def display_account_info(self):
-        print(f"Balance: ${self.balance}")
-
     def yield_interest(self):
         if self.balance > 0:
             self.balance += self.int_rate
             return self
 
-michaelAccount = BankAccount(8, 100)
-annaAccount = BankAccount(9, 100)
+michael = User("michael", "michael.com")
+anna = User("anna", "anna.com")
 
-michaelAccount.deposit(500).deposit(500).deposit(500).withdraw(100).yield_interest()
+michael.account.deposit(500).deposit(500).deposit(500).withdraw(100).yield_interest()
 
-annaAccount.deposit(500).deposit(500).withdraw(100).withdraw(100).withdraw(100).withdraw(100).yield_interest()
+anna.account.deposit(500).deposit(500).withdraw(100).withdraw(100).withdraw(100).withdraw(100).yield_interest()
